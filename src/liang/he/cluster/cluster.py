54776a173b86638cac6501d90b6ec3264e59fd95
Liang.he <liang.he@mathartsys.com>
2020-01-08 16:03:58 +0800

    fix:issue1

--- src/liang/he/cluster/cluster.py
+++ src/liang/he/cluster/cluster.py
@@ -1,14 +1,6 @@
 """K-means clustering"""
 
-# Authors: Gael Varoquaux <gael.varoquaux@normalesup.org>
-#          Thomas Rueckstiess <ruecksti@in.tum.de>
-#          James Bergstra <james.bergstra@umontreal.ca>
-#          Jan Schlueter <scikit-learn@jan-schlueter.de>
-#          Nelle Varoquaux
-#          Peter Prettenhofer <peter.prettenhofer@gmail.com>
-#          Olivier Grisel <olivier.grisel@ensta.org>
-#          Mathieu Blondel <mathieu@mblondel.org>
-#          Robert Layton <robertlayton@gmail.com>
+# Authors: Liang.he 378887701@qq.com
 # License: BSD 3 clause
 
 # 通过简单的例子来直接查看K均值聚类的效果
